372f0dfa67e83a06f564578c081bcc1132fd6a1f
ArdSherafat <ardavan.sherafat@memverge.com>
2023-08-16 13:42:21 -0700

    update docker installation

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ function install-docker()
           "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
           "$(. /etc/os-release && echo "${VERSION_CODENAME}")" stable" | \
           sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+        sudo apt-get update
         sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
         sudo docker info -f '{{ .DockerRootDir}}'
